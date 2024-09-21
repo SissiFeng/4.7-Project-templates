08e68639dfe037034c6608beb948462beb7388ca
Sissi Feng <38707787+SissiFeng@users.noreply.github.com>
2024-09-21 11:42:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='ml_predictor',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
+    include_package_data=True,
     install_requires=[
         'pandas',
         'numpy',
@@ -13,7 +14,10 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'ml_predictor=ml_predictor.model:main',
+            'ml_predictor=ml_predictor.cli:main',
         ],
     },
+    # TODO: Add project description
+    # TODO: Add author and author_email
+    # TODO: Add url to the project repository
 )
